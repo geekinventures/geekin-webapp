6bf0d3e7cdc795845f867ab27fd3883ce7d2f270
liltimtim@gmail.com <liltimtim@gmail.com>
2014-11-28 18:40:13 -0500

    added login logic and functions

--- public_html/scripts/splashcontroller.js
+++ public_html/scripts/splashcontroller.js
@@ -19,6 +19,14 @@ $(document).ready(function(){
     $("#submit-btn").click(function(event){
         event.preventDefault();
         console.log($("#username-input").val());
+        var username = $("#username-input").val();
+        var password = $("#password-input").val();
+        var user = {};
+        user.username = username;
+        user.password = password;
+        loginUser(user);
+        
+        //verify user actually entered something
         if($("#username-input").val().length === 0){
             console.log("user being too cute...need to enter text");
             alert("You gotta have a username yo!");
@@ -26,10 +34,31 @@ $(document).ready(function(){
             if(typeof Storage !== 'undefined'){
                 localStorage.setItem('user_name', $("#username-input").val());
                 console.log(localStorage.getItem('user_name'));
-                window.location = "searchview.html";
+//                window.location = "searchview.html";
             }else{
                 alert("Your browser doesn't support local storage...upgrade quick");
             }
         }
     });
 });
+
+function createNewUser(user){
+    //creates a new user with provided credentials if none exist
+}
+
+function loginUser(user){
+    console.log(user);
+    //logs in an authenticated user and redirects them to main page
+    var ref = new Firebase("https://luminous-inferno-8382.firebaseio.com");
+    ref.authWithPassword({
+        email: String(user.email),
+        password: String(user.password)
+    }, function(error, authData){
+        if(error){
+            console.log(error);
+            alert("Login Failed with error ", error);
+        }else{
+            console.log("Authenticated with authData ", authData);
+        }
+    });
+}
